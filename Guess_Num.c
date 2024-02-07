055ea6e78bf6e1d05dc8b8642c4d21efa9ca6bfd
Lord-Sinq <82123439+Lord-Sinq@users.noreply.github.com>
2024-02-07 10:05:23 -0500

    Update Guess_Num.c

--- Guess_Num.c
+++ Guess_Num.c
@@ -2,25 +2,42 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int Guessing(int* num_guess){
-	int random = rand();
+int Guessing(int num_guess){
+	/**
+	 * Takes the users number and if it does not match 
+	 * it wil tell you if you are to high or low
+	 * */
+	int random = rand() % 10 + 1;
+	int trys = 0;
+
 	while (num_guess != random){ 
-		if( num_guess == random){
-			printf("%d",num_guess);
+		trys++;
+		
+		if( num_guess < random){
+
+			printf("Too Low: %d\n",num_guess);
 		}else{
-			printf("%d",random);
+
+			printf("Too High: %d\n",random);
 		}
+
+		scanf("%d", &num_guess);
 	}
+
+	printf("You got in in :%d:- tries",trys);
 	return 0;
 }
 
 int main(void){
+	/***
+	 * Takes user input of a number
+	 * 
+	 * */
 	int num;
+
 	printf("Enter a number:");
 	scanf("%d",&num);
-	int *num1 = &num;
-	printf("Guessing a number:%d",Guessing(num1));
 
-
+	Guessing(num);
 	return 0;
 }
