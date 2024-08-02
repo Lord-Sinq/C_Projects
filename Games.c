d4819f40ee11aac690e3df3aa3cf9de172d78416
Lord-Sinq <82123439+Lord-Sinq@users.noreply.github.com>
2024-08-02 10:03:14 -0400

    Add files via upload

--- Games.c
+++ Games.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <setjmp.h>
 
 // math game
 void mathGame() {
@@ -10,6 +11,9 @@ void mathGame() {
 }
 
 // questions game
+
+#define ARRAY_SIZE 5
+
 int counter = 0;
 bool playAgain = false;
 char qgAgain;
@@ -19,48 +23,85 @@ char q3[100];
 char q4[100];
 char q5[100];
 
+// function decloration
 bool checkAnswers( char qgUserAnswer, char qgCorrectAnswer);
 
 // question game function
 void questionGame(){
 	
 	// correct answers & users answers
-	char qgCorrectAnswer[] = {'T','A','D','C','Y'};
-	char qgUserAnswer[5][100];
+	int qgUserScore = 0;
+	char qgCorrectAnswer[ARRAY_SIZE] = {'t','a','d','c','y'};
+	char qgUserAnswer[ARRAY_SIZE];
+	// copy the correct ans to the users ans array
+	memcpy(qgUserAnswer, qgCorrectAnswer, ARRAY_SIZE * sizeof(char));
 
 	// holds the logic for the points you gain and lose in the game
 	do {
 
+		int q = 0;
+		char q1UserAns[2];
+		char q2UserAns[2];
+		char q3UserAns[2];
+		char q4UserAns[2];
+		char q5UserAns[2];
+		char temp;
+
 		// game
 		printf("\n==Welcome==\nYou have 5 questions.\n");
 
-		for (int q = 1;q <= 5; q++){
-			printf("Question %d: Is the statement true or false.\n", q);
-			printf("I have a male cat. \n:");
-			scanf("%c", qgUserAnswer[q1]);
-			q++;
-			printf("Question %d: Pick the correct answer\n", q);
-			printf(" ");
-			scanf("%c", qgUserAnswer[q2]);
-			q++;
-			printf("Question %d: Pick the correct answer\n", q);
-			scanf("%c", qgUserAnswer[q3]);
-			q++;
-			printf("Question %d: Pick the correct answer\n", q);
-			scanf("%c", qgUserAnswer[q4]);
-			q++;
-			printf("Question %d: Last question\n", q);
-			printf("Do you love me????\n:");
-			scanf("%c", qgUserAnswer[q5]);
+		//q1
+		printf("Question %d: Is the statement true or false.\n", q++);
+		printf("I have a male cat. \n: ");
+		scanf(" %s", q1UserAns);
+
+		strcpy(&qgUserAnswer[1], q1UserAns);
+		//q2
+		printf("Question %d: Pick the correct answer\n: ", q++);
+		scanf(" %s", q2UserAns);
+		strcpy(&qgUserAnswer[2], q2UserAns);
+		//q3
+		printf("Question %d: Pick the correct answer\n: ", q++);
+		scanf(" %s", q3UserAns);
+		strcpy(&qgUserAnswer[3], q3UserAns);
+		//q4
+		printf("Question %d: Pick the correct answer\n: ", q++);
+		scanf(" %s", q4UserAns);
+		strcpy(&qgUserAnswer[4], q4UserAns);
+		//q5
+		printf("Question %d: Last question\n", q);
+		printf("Do you love me????\n: ");
+		scanf(" %s", q5UserAns);
+		strcpy(&qgUserAnswer[5], q5UserAns);
+
+		// clear the input buffer
+		while ((temp = getchar()) != '\n' && temp != EOF);
 
 
-			//results
-			checkAnswers(qgUserAnswer[q], qgCorrectAnswer[q]);
-
-
+		for (int q = 0; q <= 5; q++){
+			printf("q %d: ", qgUserAnswer[q]);
 		}
 
+		//check answer1
+		for (int q = 0; q <= 5; q++){
+			bool qgResult = checkAnswers(&qgUserAnswer[q], qgCorrectAnswer[q]);
+			if (qgResult){
+				qgUserScore + 20;
+				q++;
+			}else{
+				qgUserScore + 0;
+				q++;
+			}
+			printf("\nScore: %d", qgResult);
+		}
+		printf("\n\nScore: %d", qgUserScore);
+
 	} while (playAgain);
+}
+
+// function definition
+bool checkAnswers(char qgUserAnswer, char qgCorrectAnswer) {
+	return qgUserAnswer == qgCorrectAnswer;
 }
 
 void checkPlayAgain() {
@@ -105,13 +146,15 @@ int main(){
 	printf("!!!!!!!!!!!!!!!!!!!!!!!HELLO!!!!!!!!!!!!!!!!!!!!!!!!\n");
 	printf("!-------Please pick a number 1-2 for a game--------!\n");
 	printf("!--------------------------------------------------!\n");
-	printf("! (1)Question Game            (2)Next Game         !\n");
+	printf("! (1) Question Game            (2) Math Game       !\n");
 	printf("!__________________________________________________!\n");
 	scanf("%d", &userChoice);
 
 	switch (userChoice){
 		case 1:
 			do {
+				// trys setjmp code
+				//setjmp(env);
 				printf("Question game!");
 				questionGame();
 				// check if they want to play again
@@ -133,7 +176,6 @@ int main(){
 			printf("default choice\n");
 			break;
 	}
-
 
 	return 0;
 }
